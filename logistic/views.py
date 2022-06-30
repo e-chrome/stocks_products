3bd274a28828c75a55337c47f2efed6c930d77a7
Евгений Хромов <hromov.gazprom@yandex.ru>
2022-06-30 10:46:51 +0300

    added pagination and stock filter

--- logistic/views.py
+++ logistic/views.py
@@ -16,4 +16,5 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    filterset_fields = ['products',]
 
